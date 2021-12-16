fe118e76af57ae6a8a4356e04dad15fd4a2303a1
zach.hall <29452017+zachhall1234@users.noreply.github.com>
2021-12-16 13:18:29 +0000

    Customs data store spec

--- app/connectors/CustomsDataStoreConnector.scala
+++ app/connectors/CustomsDataStoreConnector.scala
@@ -19,8 +19,10 @@ package connectors
 import config.FrontendAppConfig
 import javax.inject.Inject
 import models.CompanyInformation
+import play.api.libs.json.Json
 import uk.gov.hmrc.http.HttpReads.Implicits._
 import uk.gov.hmrc.http.{HeaderCarrier, HttpClient, HttpErrorFunctions}
+
 import scala.concurrent.{ExecutionContext, Future}
 
 class CustomsDataStoreConnector @Inject()(appConfig: FrontendAppConfig,
