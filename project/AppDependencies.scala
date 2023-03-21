4b34489290f55e9de98a085f64cb6d88dc348472
wg-hmrc <waigwa.gatamu@digital.hmrc.gov.uk>
2023-03-21 15:01:56 +0000

    revert

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,9 +3,10 @@ import play.core.PlayVersion
 
 object AppDependencies {
 
-  private val bootstrapVersion = "5.16.0"
+  private val bootstrapVersion = "5.24.0"
 
   val compile = Seq(
+     play.sbt.PlayImport.ws,
     "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % bootstrapVersion,
     "uk.gov.hmrc" %% "play-frontend-hmrc" % "6.8.0-play-28",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28" % "0.71.0",
@@ -13,7 +14,6 @@ object AppDependencies {
   )
 
   val test = Seq(
-    "uk.gov.hmrc" %% "bootstrap-test-play-28" % bootstrapVersion,
     "org.scalatest" %% "scalatest" % "3.0.8",
     "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.2",
     "org.pegdown" % "pegdown" % "1.6.0",
