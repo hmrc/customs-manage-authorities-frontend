adf9b30a87e45483ab323f58cb5add9fefa32026
wg-hmrc <107496826+wg-hmrc@users.noreply.github.com>
2023-03-23 11:23:53 +0000

    Validation does not accommodate lowercase (#88)

--- app/forms/EoriNumberFormProvider.scala
+++ app/forms/EoriNumberFormProvider.scala
@@ -26,7 +26,6 @@ class EoriNumberFormProvider @Inject() extends Mappings {
   def apply(): Form[String] =
     Form(
       "value" -> text("eoriNumber.error.required")
-        .verifying(checkEORI("eoriNumber.error.format"))
     )
 
 }
