1b0d805ae3d00357b599ad18744b6d6352616b62
Adam Cooper <61467301+AdamCoops@users.noreply.github.com>
2023-03-23 11:47:47 +0000

    MMCA-3865-Update test app depends

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -13,15 +13,15 @@ object AppDependencies {
   )
 
   val test = Seq(
-    "uk.gov.hmrc" %% "bootstrap-test-play-28" % "5.24.0",
-    "org.scalatest" %% "scalatest" % "3.0.8",
-    "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.2",
-    "org.pegdown" % "pegdown" % "1.6.0",
-    "org.jsoup" % "jsoup" % "1.12.1",
-    "com.typesafe.play" %% "play-test" % PlayVersion.current,
-    "org.mockito" % "mockito-all" % "1.10.19",
-    "org.scalacheck" %% "scalacheck" % "1.14.1",
-    "com.github.tomakehurst" % "wiremock-standalone" % "2.25.0"
+    "uk.gov.hmrc" %% "bootstrap-test-play-28" % "5.24.0" % Test,
+    "org.scalatest" %% "scalatest" % "3.1.0" % Test,
+    "org.scalatestplus.play" %% "scalatestplus-play" % "5.1.0" % Test,
+    "org.pegdown" % "pegdown" % "1.6.0" % Test,
+    "org.jsoup" % "jsoup" % "1.12.1" % Test,
+    "com.typesafe.play" %% "play-test" % PlayVersion.current % Test,
+    "org.mockito" % "mockito-all" % "1.10.19" % Test,
+    "org.scalacheck" %% "scalacheck" % "1.14.1" % Test,
+    "com.github.tomakehurst" % "wiremock-standalone" % "2.25.0" % Test
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = compile ++ test
