d53533217f5500fe58427786be94aa1f5fb16ca4
sameerhmrc <106383455+sameerhmrc@users.noreply.github.com>
2023-05-03 16:24:52 +0100

    MMCA-3980 : Validation does not accommodate lowercase

--- test/forms/mappings/ConstraintsSpec.scala
+++ test/forms/mappings/ConstraintsSpec.scala
@@ -216,7 +216,7 @@ class ConstraintsSpec extends WordSpec with MustMatchers with ScalaCheckProperty
 
     "return Invalid when an incorrect EORI format is provided" in {
       val result = checkEORI("error.invalid2")("XI453")
-      result mustEqual Invalid("error.invalid2", """([Gg][Bb])\d{12}""")
+      result mustEqual Invalid("error.invalid2", """([Gg][Bb])\\d{12}""")
     }
 
     "return Valid for an input that does not match the expression" in {
