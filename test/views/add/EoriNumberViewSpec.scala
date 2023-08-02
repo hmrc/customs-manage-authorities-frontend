41698c741d4443bae78894bf893272b3af0579eb
tyagikumarvineet <tyagi.kumar.vineet@gmail.com>
2023-08-02 15:55:11 +0100

    [MMCA-4159] Add tests, update msgs and move postion of the guidance in the view

---
+++ test/views/add/EoriNumberViewSpec.scala
@@ -0,0 +1,101 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views.add
+
+import base.SpecBase
+import config.FrontendAppConfig
+import forms.EoriNumberFormProvider
+import models.{CheckMode, NormalMode}
+import org.jsoup.Jsoup
+import org.jsoup.select.Elements
+import org.mockito.Mockito.when
+import org.scalatestplus.mockito.MockitoSugar
+import play.api.Application
+import play.api.i18n.Messages
+import play.api.mvc.{AnyContentAsEmpty, Call}
+import play.api.test.{FakeRequest, Helpers}
+import services.DateTimeService
+import views.html.add.EoriNumberView
+
+import java.time.LocalDateTime
+
+class EoriNumberViewSpec extends SpecBase with MockitoSugar {
+
+  "EoriNumberView" should {
+    "display the correct guidance in Normal mode" in new Setup {
+      val formElements: Elements = normalModeView().getElementsByTag("form")
+      formElements.get(0).childNodes().get(3).hasAttr("data-module") mustBe true
+
+
+      normalModeView().title() mustBe "eoriNumber.title - service.name - site.govuk"
+      normalModeView().getElementsByClass("govuk-details__summary-text").html() mustBe
+        "eoriNumber.details.label"
+      normalModeView().getElementsByClass("govuk-details__text").html() mustBe
+        "eoriNumber.details.text"
+
+      normalModeView().getElementsByClass("govuk-hint").html() mustBe "eoriNumber.hint"
+      normalModeView().getElementsByTag("button").get(1).html() mustBe "site.saveAndContinue"
+    }
+
+    "display the correct guidance in Check mode" in new Setup {
+      val formElements: Elements = normalModeView().getElementsByTag("form")
+      formElements.get(0).childNodes().get(3).hasAttr("data-module") mustBe true
+
+      checkModeView().title() mustBe "eoriNumber.title - service.name - site.govuk"
+      checkModeView().getElementsByClass("govuk-details__summary-text").html() mustBe
+        "eoriNumber.details.label"
+      checkModeView().getElementsByClass("govuk-details__text").html() mustBe
+        "eoriNumber.details.text"
+
+      normalModeView().getElementsByClass("govuk-hint").html() mustBe "eoriNumber.hint"
+      normalModeView().getElementsByTag("button").get(1).html() mustBe "site.saveAndContinue"
+    }
+
+    "when back-link is clicked returns to previous page on Normal Mode" in new Setup {
+      normalModeView().getElementsByClass("govuk-back-link").attr("href") mustBe
+        s"/customs/manage-authorities/manage-account-authorities"
+    }
+
+    "when back-link is clicked returns to previous page on Check Mode" in new Setup {
+      checkModeView().getElementsByClass("govuk-back-link").attr("href") mustBe
+        s"/customs/manage-authorities/add-authority/check-answers"
+    }
+  }
+
+  trait Setup  {
+    val mockDateTimeService: DateTimeService = mock[DateTimeService]
+    when(mockDateTimeService.localTime()).thenReturn(LocalDateTime.now())
+
+    implicit val csrfRequest: FakeRequest[AnyContentAsEmpty.type] =
+      fakeRequest("GET", "/some/resource/path")
+    val app: Application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+    implicit val appConfig: FrontendAppConfig = app.injector.instanceOf[FrontendAppConfig]
+    implicit val messages: Messages = Helpers.stubMessages()
+
+    private val formProvider = new EoriNumberFormProvider()
+    private val form = formProvider()
+
+    private lazy val normalModeBackLinkRoute: Call = controllers.routes.ManageAuthoritiesController.onPageLoad
+    private lazy val checkModeBackLinkRoute: Call = controllers.add.routes.AuthorisedUserController.onPageLoad()
+
+
+    def normalModeView() = Jsoup.parse(
+      app.injector.instanceOf[EoriNumberView].apply(form,NormalMode,normalModeBackLinkRoute).body)
+    def checkModeView() = Jsoup.parse(
+      app.injector.instanceOf[EoriNumberView].apply(form,CheckMode,checkModeBackLinkRoute).body)
+  }
+}
