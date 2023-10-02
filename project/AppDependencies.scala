48602a602a075f3b748fcffb64b5a2c89395dae7
sameerhmrc <106383455+sameerhmrc@users.noreply.github.com>
2023-10-02 12:11:40 +0100

    MMCA-4262 : Update version & configuration for CDS Financials service - Sept - V6 (#157)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,12 +3,12 @@ import play.core.PlayVersion
 
 object AppDependencies {
 
-  private val bootstrapVersion = "7.19.0"
+  private val bootstrapVersion = "7.22.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % bootstrapVersion,
-    "uk.gov.hmrc" %% "play-frontend-hmrc" % "7.14.0-play-28",
+    "uk.gov.hmrc" %% "play-frontend-hmrc" % "7.21.0-play-28",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28" % "1.3.0",
     "org.typelevel" %% "cats-core" % "2.9.0"
   )
@@ -26,7 +26,9 @@ object AppDependencies {
     "org.mockito" % "mockito-all" % "1.10.19",
     "org.scalacheck" %% "scalacheck" % "1.17.0",
     "com.github.tomakehurst" % "wiremock-standalone" % "2.27.2",
-    "com.vladsch.flexmark" % "flexmark-all" % "0.36.8"
+    "com.vladsch.flexmark" % "flexmark-all" % "0.36.8",
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28" % "1.3.0",
+    "uk.gov.hmrc" %% "play-frontend-hmrc" % "7.21.0-play-28"
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = compile ++ test
