f36fb042080c854997b828f1d0118b74b9bb7a95
tyagikumarvineet <tyagi.kumar.vineet@gmail.com>
2023-10-05 23:36:24 +0100

    [MMCA-4285] Add new tests

---
+++ test/controllers/TechnicalDifficultiesSpec.scala
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import base.SpecBase
+import config.FrontendAppConfig
+import play.api.Application
+import play.api.http.Status.OK
+import play.api.test.FakeRequest
+import play.api.test.Helpers.{GET, contentAsString, defaultAwaitTimeout, route, running, status, writeableOf_AnyContentAsEmpty}
+import views.html.ErrorTemplate
+
+class TechnicalDifficultiesSpec extends SpecBase {
+
+  "onPageLoad" should {
+    "return OK" in new Setup {
+      running(app) {
+        val request = FakeRequest(GET, routes.TechnicalDifficulties.onPageLoad.url)
+
+        val result = route(app, request).value
+
+
+        status(result) mustBe OK
+        val contentAsStringResult = contentAsString(result)
+
+        contentAsStringResult contains "service-technical-difficulties.title"
+        contentAsStringResult contains "service.technical-difficulties.heading"
+        contentAsStringResult contains "service.technical-difficulties.p"
+
+
+        /*contentAsString(result) mustBe view("service-technical-difficulties.title",
+          "service.technical-difficulties.heading",
+          "service.technical-difficulties.p")(fakeRequest(), msgs, config)
+*/
+      }
+    }
+  }
+
+  trait Setup {
+    val app: Application = applicationBuilder().build()
+    implicit val config = app.injector.instanceOf[FrontendAppConfig]
+    implicit val msgs = messages(app)
+    val view = app.injector.instanceOf[ErrorTemplate]
+  }
+}
---
+++ test/models/domain/AccountTypeSpec.scala
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models.domain
+
+import base.SpecBase
+import play.api.libs.json.{JsSuccess, Json}
+
+class AccountTypeSpec extends SpecBase {
+
+  "AccountTypeReads" should {
+    "output the correct value" in {
+      import models.domain.AccountType.AccountTypeReads
+
+      Json.fromJson(Json.parse("\"CDSCash\"")) mustBe JsSuccess(CdsCashAccount)
+      Json.fromJson(Json.parse("\"DutyDeferment\"")) mustBe JsSuccess(CdsDutyDefermentAccount)
+      Json.fromJson(Json.parse("\"GeneralGuarantee\"")) mustBe JsSuccess(CdsGeneralGuaranteeAccount)
+      Json.fromJson(Json.parse("\"Invalid\"")) mustBe JsSuccess(UnknownAccount)
+    }
+  }
+
+  "accountTypeWrites" should {
+    "output the correct value" in {
+      import models.domain.AccountType.accountTypeWrites
+
+      Json.toJson(accountTypeWrites.writes(CdsCashAccount)) mustBe Json.parse("\"CDSCash\"")
+      Json.toJson(accountTypeWrites.writes(CdsDutyDefermentAccount)) mustBe Json.parse("\"DutyDeferment\"")
+      Json.toJson(accountTypeWrites.writes(CdsGeneralGuaranteeAccount)) mustBe Json.parse("\"GeneralGuarantee\"")
+      Json.toJson(accountTypeWrites.writes(UnknownAccount)) mustBe Json.parse("\"UnknownAccount\"")
+    }
+  }
+}
