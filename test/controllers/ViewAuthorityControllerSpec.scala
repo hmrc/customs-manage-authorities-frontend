1dec46cfd0a61dc7315bf432d21d63edc66b2412
tyagikumarvineet <tyagi.kumar.vineet@gmail.com>
2023-10-06 18:04:29 +0100

    [MMCA-4285] Add new test classes and add .DS_Store in gitignore

--- test/controllers/ViewAuthorityControllerSpec.scala
+++ test/controllers/ViewAuthorityControllerSpec.scala
@@ -16,21 +16,27 @@
 
 package controllers
 
-import java.time.LocalDate
-
 import base.SpecBase
 import models.CompanyDetails
 import models.domain._
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito.when
+import org.scalatestplus.mockito.MockitoSugar.mock
 import pages.add.{AccountsPage, AuthorityStartDatePage, EoriNumberPage}
+import play.api.Application
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
+import services.{AccountAndAuthority, AuthoritiesCacheService, NoAuthority}
+
+import java.time.LocalDate
+import scala.concurrent.Future
 
 class ViewAuthorityControllerSpec extends SpecBase {
 
-  "View Authority Controller" must {
-    ".onPageLoad calling any empty values gives you 404" in new Setup {
+  "onPageLoad" must {
+    "return 404 when calling with empty values" in new Setup {
 
-      val application = applicationBuilder(userAnswers = Some(userAnswers)).build()
+      val application: Application = applicationBuilder(userAnswers = Some(userAnswers)).build()
 
       running(application) {
         val request = FakeRequest(
@@ -41,21 +47,64 @@ class ViewAuthorityControllerSpec extends SpecBase {
         status(result) mustEqual NOT_FOUND
       }
     }
+
+    "return OK when called with correct values" in new Setup {
+
+      val application: Application = applicationBuilder(userAnswers = Some(userAnswers)).build()
+
+      when(mockAuthCacheService.getAccountAndAuthority(any(), any(), any())(any()))
+        .thenReturn(Future.successful(Right(AccountAndAuthority(accountsWithAuthoritiesWithId, standingAuthority))))
+
+      running(application) {
+        val request = FakeRequest(
+          GET, controllers.routes.ViewAuthorityController.onPageLoad(
+            "test_account", "test_id").url)
+
+        val result = route(application, request).value
+        //TODO: To be updated to OK
+        status(result) mustEqual SEE_OTHER
+      }
+    }
+
+    "return 303 when AuthoritiesCacheErrorResponse is received" in new Setup {
+
+      val application: Application = applicationBuilder(userAnswers = Some(userAnswers)).build()
+
+      when(mockAuthCacheService.getAccountAndAuthority(any(), any(), any())(any()))
+        .thenReturn(Future.successful(Left(NoAuthority)))
+
+      running(application) {
+        val request = FakeRequest(
+          GET, controllers.routes.ViewAuthorityController.onPageLoad(
+            "test_account", "test_id").url)
+
+        val result = route(application, request).value
+        status(result) mustEqual SEE_OTHER
+      }
+    }
   }
-}
 
-trait Setup extends SpecBase {
+  trait Setup {
 
-  val startDate = LocalDate.now().plusMonths(1)
+    val startDate = LocalDate.now().plusMonths(1)
 
-  val cashAccount = CashAccount(
-    "12345", "GB123456789012", AccountStatusOpen, CDSCashBalance(Some(100.00)))
+    val cashAccount = CashAccount(
+      "12345", "GB123456789012", AccountStatusOpen, CDSCashBalance(Some(100.00)))
 
-  val dutyDeferment = DutyDefermentAccount(
-    "67890", "GB210987654321", AccountStatusOpen, DutyDefermentBalance(None, None, None, None))
+    val dutyDeferment = DutyDefermentAccount(
+      "67890", "GB210987654321", AccountStatusOpen, DutyDefermentBalance(None, None, None, None))
 
-  val userAnswers = emptyUserAnswers
-    .set(EoriNumberPage, CompanyDetails("GB123456789012", Some("Company Name"))).success.value
-    .set(AuthorityStartDatePage, startDate).success.value
-    .set(AccountsPage, List(cashAccount, dutyDeferment)).success.value
+    val userAnswers = emptyUserAnswers
+      .set(EoriNumberPage, CompanyDetails("GB123456789012", Some("Company Name"))).success.value
+      .set(AuthorityStartDatePage, startDate).success.value
+      .set(AccountsPage, List(cashAccount, dutyDeferment)).success.value
+
+    val stDate: LocalDate = LocalDate.parse("2020-03-01")
+    val endDate: LocalDate = LocalDate.parse("2020-04-01")
+    val standingAuthority: StandingAuthority = StandingAuthority("EORI", startDate, Some(endDate), viewBalance = false)
+    val accountsWithAuthoritiesWithId: AccountWithAuthoritiesWithId =
+      AccountWithAuthoritiesWithId(CdsCashAccount, "12345", Some(AccountStatusOpen), Map("b" -> standingAuthority))
+
+    val mockAuthCacheService = mock[AuthoritiesCacheService]
+  }
 }
--- test/models/CustomsAccountsSpec.scala
+++ test/models/CustomsAccountsSpec.scala
@@ -16,20 +16,27 @@
 
 package models
 
-import models.domain.{AccountStatusOpen, CDSAccountStatus, GeneralGuaranteeAccount, GeneralGuaranteeBalance}
-import org.scalatest._
-import models.domain.CDSAccountStatus._
+import base.SpecBase
+import models.domain.{AccountStatusClosed, AccountStatusOpen, AccountStatusPending, AccountStatusSuspended,
+  CDSAccounts, CDSCashBalance, CashAccount, DutyDefermentAccount, DutyDefermentBalance, GeneralGuaranteeAccount,
+  GeneralGuaranteeBalance}
 import play.api.libs.json._
 
-class CustomsAccountsSpec extends WordSpec with MustMatchers {
+class CustomsAccountsSpec extends SpecBase {
 
   private val traderEori = "12345678"
 
-  val guaranteeAccount = GeneralGuaranteeAccount("G123456", traderEori, AccountStatusOpen, Some(GeneralGuaranteeBalance(BigDecimal(1000000), BigDecimal(200000))))
-  val guaranteeAccountZeroLimit = GeneralGuaranteeAccount("G123456", traderEori, AccountStatusOpen, Some(GeneralGuaranteeBalance(BigDecimal(0), BigDecimal(200001))))
-  val guaranteeAccountZeroBalance = GeneralGuaranteeAccount("G123456", traderEori, AccountStatusOpen, Some(GeneralGuaranteeBalance(BigDecimal(200002), BigDecimal(0))))
-  val guaranteeAccountZeroLimitZeroBalance = GeneralGuaranteeAccount("G123456", traderEori, AccountStatusOpen, Some(GeneralGuaranteeBalance(BigDecimal(0), BigDecimal(0))))
+  val guaranteeAccount: GeneralGuaranteeAccount = GeneralGuaranteeAccount("G123456", traderEori,
+    AccountStatusOpen, Some(GeneralGuaranteeBalance(BigDecimal(1000000), BigDecimal(200000))))
 
+  val guaranteeAccountZeroLimit: GeneralGuaranteeAccount = GeneralGuaranteeAccount("G123456", traderEori,
+    AccountStatusOpen, Some(GeneralGuaranteeBalance(BigDecimal(0), BigDecimal(200001))))
+
+  val guaranteeAccountZeroBalance: GeneralGuaranteeAccount = GeneralGuaranteeAccount("G123456", traderEori,
+    AccountStatusOpen, Some(GeneralGuaranteeBalance(BigDecimal(200002), BigDecimal(0))))
+
+  val guaranteeAccountZeroLimitZeroBalance: GeneralGuaranteeAccount = GeneralGuaranteeAccount("G123456", traderEori,
+    AccountStatusOpen, Some(GeneralGuaranteeBalance(BigDecimal(0), BigDecimal(0))))
 
   "GeneralGuaranteeBalance" should {
 
@@ -60,10 +67,74 @@ class CustomsAccountsSpec extends WordSpec with MustMatchers {
 
   }
 
+  "DutyDefermentAccount" should {
+    "return correct order after compare while sorting" in {
+      val ddAccn1 = DutyDefermentAccount("123", "XI9876543210000", AccountStatusOpen,
+        DutyDefermentBalance(Some(100.00), Some(100.00), Some(100.00), Some(100.00)), isNiAccount = true)
+
+      val ddAccn2 = DutyDefermentAccount("124", "XI9876543210000", AccountStatusOpen,
+        DutyDefermentBalance(Some(100.00), Some(100.00), Some(100.00), Some(100.00)), isNiAccount = true)
+
+      ddAccn2 > ddAccn1 mustBe true
+      ddAccn1.accountType mustBe "dutyDeferment"
+    }
+  }
+
+  "CashAccount" should {
+    "return correct accountType value" in {
+      val openCashAccount: CashAccount =
+        CashAccount("23456", "GB123456789012", AccountStatusClosed, CDSCashBalance(Some(100.00)))
+
+      openCashAccount.accountType mustBe "cash"
+    }
+  }
+
   "CDSAccountStatusReads and CDSAccountStatusWrites" should {
-    "return the correct result" in {
-      Json.fromJson[CDSAccountStatus](Json.toJson(AccountStatusOpen)).get.mustEqual(AccountStatusOpen)
-      Json.toJson(AccountStatusOpen).validate.get mustBe AccountStatusOpen
+    "return the correct output for CDSAccountStatusReads" in {
+      import models.domain.CDSAccountStatus.CDSAccountStatusReads
+
+      Json.fromJson(Json.parse("\"Open\"")) mustBe JsSuccess(AccountStatusOpen)
+      Json.fromJson(Json.parse("\"Suspended\"")) mustBe JsSuccess(AccountStatusSuspended)
+      Json.fromJson(Json.parse("\"Closed\"")) mustBe JsSuccess(AccountStatusClosed)
+      Json.fromJson(Json.parse("\"Pending\"")) mustBe JsSuccess(AccountStatusPending)
+      Json.fromJson(Json.parse("\"Invalid\"")) mustBe JsSuccess(AccountStatusOpen)
+    }
+
+    "return the correct output for CDSAccountStatusWrites" in {
+      import models.domain.CDSAccountStatus.CDSAccountStatusWrites
+
+      CDSAccountStatusWrites.writes(AccountStatusOpen) mustBe Json.parse("\"Open\"")
+      CDSAccountStatusWrites.writes(AccountStatusClosed) mustBe Json.parse("\"Closed\"")
+      CDSAccountStatusWrites.writes(AccountStatusSuspended) mustBe Json.parse("\"Suspended\"")
+      CDSAccountStatusWrites.writes(AccountStatusPending) mustBe Json.parse("\"Pending\"")
+    }
+  }
+
+  "CDSAccounts.alreadyAuthorised" should {
+    "return the correct output" in {
+
+      val openCashAccount: CashAccount =
+        CashAccount("23456", "GB123456789012", AccountStatusClosed, CDSCashBalance(Some(100.00)))
+      val closedCashAccount: CashAccount =
+        CashAccount("12345", "GB123456789012", AccountStatusOpen, CDSCashBalance(Some(100.00)))
+
+      val cdsAccounts: CDSAccounts = CDSAccounts("GB123456789012", List(openCashAccount, closedCashAccount))
+
+      cdsAccounts.alreadyAuthorised(Seq("12345")) mustBe Seq(closedCashAccount)
+    }
+  }
+
+  "CDSAccounts.canAuthoriseAccounts" should {
+    "return the correct output" in {
+
+      val openCashAccount: CashAccount =
+        CashAccount("23456", "GB123456789012", AccountStatusClosed, CDSCashBalance(Some(100.00)))
+      val closedCashAccount: CashAccount =
+        CashAccount("12345", "GB123456789012", AccountStatusOpen, CDSCashBalance(Some(100.00)))
+
+      val cdsAccounts: CDSAccounts = CDSAccounts("GB123456789012", List(openCashAccount, closedCashAccount))
+
+      cdsAccounts.canAuthoriseAccounts(Seq("12345")) mustBe Seq()
     }
   }
 
