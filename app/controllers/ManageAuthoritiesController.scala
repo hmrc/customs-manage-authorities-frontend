690699132023bc3a263a3644233679ed214b2097
tyagikumarvineet <tyagi.kumar.vineet@gmail.com>
2024-01-26 11:26:23 +0000

    [MMCA-4452] Incorporate review comment

--- app/controllers/ManageAuthoritiesController.scala
+++ app/controllers/ManageAuthoritiesController.scala
@@ -77,8 +77,8 @@ class ManageAuthoritiesController @Inject()(
   }
 
   private def getAllAccounts(eori: EORI,
-                             xiEori: Option[String])(
-                              implicit request: IdentifierRequest[AnyContent]): Future[CDSAccounts] = {
+                             xiEori: Option[String])
+                            (implicit request: IdentifierRequest[AnyContent]): Future[CDSAccounts] = {
     val eoriList = Seq(eori, xiEori.getOrElse(emptyString)).filterNot(_ == emptyString)
 
     for {
