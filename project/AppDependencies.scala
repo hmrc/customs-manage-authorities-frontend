f61eba9f4decb679bd080fad22660fff4defa91a
Adam Cooper <adam.cooper2@digital.hmrc.gov.uk>
2024-04-16 10:02:19 +0100

    MMCA-4547-comment out unrequired dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -14,19 +14,19 @@ object AppDependencies {
   )
 
   val test: Seq[ModuleID] = Seq(
-    "org.scalatest" %% "scalatest" % "3.2.16",
+    //"org.scalatest" %% "scalatest" % "3.2.16",
     "uk.gov.hmrc" %% "bootstrap-test-play-30" % bootstrapVersion,
-    "org.scalatestplus.play" %% "scalatestplus-play" % "7.0.1",
+    //"org.scalatestplus.play" %% "scalatestplus-play" % "7.0.1",
     "org.scalatestplus" %% "mockito-3-4" % "3.2.10.0",
     "org.scalatestplus" %% "scalacheck-1-15" % "3.2.11.0",
     "org.mockito" % "mockito-core" % "5.11.0",
     "org.pegdown" % "pegdown" % "1.6.0",
-    "org.jsoup" % "jsoup" % "1.16.1",
+    //"org.jsoup" % "jsoup" % "1.16.1",
     "org.playframework" %% "play-test" % PlayVersion.current,
     "org.mockito" % "mockito-all" % "1.10.19",
-    "org.scalacheck" %% "scalacheck" % "1.17.0",
+    //"org.scalacheck" %% "scalacheck" % "1.17.0",
     "com.github.tomakehurst" % "wiremock-standalone" % "2.27.2",
-    "com.vladsch.flexmark" % "flexmark-all" % "0.36.8",
+    //"com.vladsch.flexmark" % "flexmark-all" % "0.36.8",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30" % "1.8.0"
   ).map(_ % Test)
 
