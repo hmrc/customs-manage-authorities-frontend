c1c49013cd08edf7698e96e6234299fce551edcb
wg-hmrc <107496826+wg-hmrc@users.noreply.github.com>
2024-05-28 14:21:06 +0100

    [MMCA-4666] Audit and change the order of the text on the confirmation (#193)
    * [MMCA-4666] Audit and change the order of the text on the confirmation
    
    * [MMCA-4666] Applied PR feedback comments
    
    * [MMCA-4666] updating removeConfirmPanel file
    
    * [MMCA-4666] Changing to single line
    
    * [MMCA-4666] Adding line break

---
+++ test/views/components/EditConfirmPanelSpec.scala
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views.components
+
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.select.Elements
+import play.api.Application
+import play.api.i18n.Messages
+import play.api.test.Helpers.running
+import base.SpecBase
+import views.html.components.editConfirmPanel
+
+class EditConfirmPanelSpec extends SpecBase {
+
+  "EditConfirmPanel view" should {
+    "display the correct title, header and company name when company name is provided" in new SetUp {
+
+      override val view: Document = Jsoup.parse(
+        app.injector
+          .instanceOf[editConfirmPanel]
+          .apply(Option("TestCompany"), Option("GB123456789000"))
+          .body
+      )
+
+      running(app) {
+        view.getElementsByTag("h1").html() mustBe
+          messages(app)("editConfirmation.heading")
+
+        firstElement.getElementsByTag("p").get(0).html() must include(
+          messages(app)("editConfirmation.body.eori.number")
+        )
+        firstElement
+          .getElementsByTag("strong")
+          .get(0)
+          .html() mustBe "GB123456789000"
+
+        firstElement.getElementsByTag("p").get(1).html() must include(
+          messages(app)("editConfirmation.body.company.name")
+        )
+        firstElement
+          .getElementsByTag("strong")
+          .get(1)
+          .html() mustBe "TestCompany"
+
+        firstElement.getElementsByTag("p").last().html() must include(
+          messages(app)("editConfirmation.body.changes")
+        )
+      }
+    }
+
+    "display the correct title, header without company name when company name is not provided" in new SetUp {
+
+      override val view: Document = Jsoup.parse(
+        app.injector
+          .instanceOf[editConfirmPanel]
+          .apply(None, Option("GB123456789000"))
+          .body
+      )
+
+      running(app) {
+        view.getElementsByTag("h1").html() mustBe
+          messages(app)("editConfirmation.heading")
+
+        firstElement.getElementsByTag("p").get(0).html() must include(
+          messages(app)("editConfirmation.body.eori.number")
+        )
+        firstElement
+          .getElementsByTag("strong")
+          .get(0)
+          .html() mustBe "GB123456789000"
+
+        firstElement.getElementsByTag("p").last().html() must include(
+          messages(app)("editConfirmation.body.changes")
+        )
+      }
+    }
+  }
+
+  trait SetUp {
+    val app: Application = applicationBuilder().build()
+    implicit val msg: Messages = messages(app)
+
+    val view: Document
+
+    lazy val bodyElements: Elements = view.getElementsByClass("govuk-panel__body-s")
+    lazy val firstElement = bodyElements.get(0)
+  }
+}
