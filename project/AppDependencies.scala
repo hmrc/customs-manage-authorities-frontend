d3e796bf4ecd8126d54b8ef4c78d2a52e12ed067
James Crook <48724197+JoniBach@users.noreply.github.com>
2025-06-06 14:49:03 +0100

    MMCA-5362: Resolve issues raised by platops pr bot

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,13 +2,13 @@ import sbt.*
 
 object AppDependencies {
 
-  val bootstrapVersion = "9.11.0"
+  val bootstrapVersion = "9.13.0"
   private val mongoVersion = "2.6.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc" %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "12.0.0",
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "12.1.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30" % mongoVersion,
     "uk.gov.hmrc" %% "play-partials-play-30" % "10.0.0",
     "org.typelevel" %% "cats-core" % "2.10.0"
